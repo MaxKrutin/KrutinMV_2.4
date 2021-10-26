c2fa8fc1f5bc63ba462e7e95efbb6231ce9059a4
Max Krutin <maxkrutin@icloud.com>
2021-10-26 16:09:53 +0300

    Initial Commit

---
+++ KrutinMV_2.3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KrutinMV_2.3
+//
+//  Created by Максим Крутинь on 26.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
